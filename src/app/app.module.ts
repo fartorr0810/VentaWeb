7965dfd6c2c592965204496c1b86d6f067bd7715
gronelog <frankarroyop@gmai.com>
2023-11-01 15:55:35 +0100

    proyecto personal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { QuienesSomosComponent } from './quienes-somos/quienes-somos.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuienesSomosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DashboardModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports:[]
 })
 export class AppModule { }
---
+++ src/app/quienes-somos/quienes-somos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quienes-somos',
+  templateUrl: './quienes-somos.component.html',
+  styleUrls: ['./quienes-somos.component.css']
+})
+export class QuienesSomosComponent {
+
+  
+}
